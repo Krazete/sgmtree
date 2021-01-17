993f8e36f211f1fb4f88090418857a0a7caa3401
Ted Michael Celis <krazete@gmail.com>
2021-01-16 20:29:54 -0800

    added image helper

--- helper.js
+++ helper.js
@@ -8,6 +8,8 @@ function polar(r, deg, p) {
     return x + " " + y;
 }
 
+/* Node Areas */
+
 function polarect(r1, r2, deg1, deg2, p) {
     var points = [
         polar(r1, deg1, p),
@@ -34,45 +36,94 @@ function polararc(r1, r2, deg1, deg2, p) {
     ].join(" ");
 }
 
-function polarectset(r1, r2, deg1, deg2, n, p) {
+function polararcset(r1, r2, deg1, deg2, n, p) {
     for (var i = 0; i < n; i++) {
         var d1 = (deg2 - deg1) * i / n;
         var d2 = (deg2 - deg1) * (i + 1) / n;
         var polygon = polararc(r1, r2, deg1 + d1, deg1 + d2, p); /* or polarect */
         console.log(polygon);
-        document.documentElement.innerHTML += polygon;
+        // document.documentElement.innerHTML += polygon;
     }
 }
 
 console.log("Ability");
-polarectset(10, 20, 54, 126, 1, 6);
-polarectset(20, 30, 54, 126, 3, 6);
-polarectset(30, 40, 54, 126, 3, 6);
-polarectset(40, 50, 54, 126, 3, 6);
+polararcset(10, 20, 54, 126, 1, 6);
+polararcset(20, 30, 54, 126, 3, 6);
+polararcset(30, 40, 54, 126, 3, 6);
+polararcset(40, 50, 54, 126, 3, 6);
 
 console.log("Attack");
-polarectset(10, 20, 126, 198, 1, 6);
-polarectset(20, 30, 126, 198, 3, 6);
-polarectset(30, 40, 126, 198, 5, 6);
-polarectset(40, 50, 126, 198, 5, 6);
+polararcset(10, 20, 126, 198, 1, 6);
+polararcset(20, 30, 126, 198, 3, 6);
+polararcset(30, 40, 126, 198, 5, 6);
+polararcset(40, 50, 126, 198, 5, 6);
 
 console.log("Move");
-polarectset(10, 20, 198, 270, 1, 6);
-polarectset(20, 30, 198, 270, 3, 6);
-polarectset(30, 40, 198, 270, 4, 6);
-polarectset(40, 50, 198, 270, 5, 6);
+polararcset(10, 20, 198, 270, 1, 6);
+polararcset(20, 30, 198, 270, 3, 6);
+polararcset(30, 40, 198, 270, 4, 6);
+polararcset(40, 50, 198, 270, 5, 6);
 
 console.log("Health");
-polarectset(10, 20, 270, 342, 1, 6);
-polarectset(20, 30, 270, 342, 3, 6);
-polarectset(30, 40, 270, 342, 5, 6);
-polarectset(40, 50, 270, 342, 5, 6);
+polararcset(10, 20, 270, 342, 1, 6);
+polararcset(20, 30, 270, 342, 3, 6);
+polararcset(30, 40, 270, 342, 5, 6);
+polararcset(40, 50, 270, 342, 5, 6);
 
 console.log("Energy");
-polarectset(10, 20, 342, 414, 1, 6);
-polarectset(20, 30, 342, 414, 3, 6);
-polarectset(30, 40, 342, 414, 5, 6);
-polarectset(40, 50, 342, 414, 5, 6);
+polararcset(10, 20, 342, 414, 1, 6);
+polararcset(20, 30, 342, 414, 3, 6);
+polararcset(30, 40, 342, 414, 5, 6);
+polararcset(40, 50, 342, 414, 5, 6);
 
 console.log("Marquee");
-polarectset(0, 10, 15, 375, 12, 6);
+polararcset(0, 10, 15, 375, 12, 6);
+
+/* Node Images */
+
+function polarimg(r, deg, p) {
+    var point = polar(r, deg, p);
+    var xy = point.split(",");
+    var x = xy[0];
+    var y = xy[1];
+    return "<image href=\"unused/SkillTreeNode-Super.png\" x=\"" + x+ "\" y=\"" + y + "\" transform=\"translate(-5,-5)\" />";
+}
+
+function polarimgset(r, deg1, deg2, n, p) {
+    for (var i = 0; i < n; i++) {
+        var d = (deg2 - deg1) * (i + 0.5) / n;
+        var img = polarimg(r, deg1 + d, p);
+        console.log(img);
+        // document.documentElement.innerHTML += img;
+    }
+}
+
+console.log("Ability");
+polarimgset(15, 54, 126, 1, 6);
+polarimgset(25, 54, 126, 3, 6);
+polarimgset(35, 54, 126, 3, 6);
+polarimgset(45, 54, 126, 3, 6);
+
+console.log("Attack");
+polarimgset(15, 126, 198, 1, 6);
+polarimgset(25, 126, 198, 3, 6);
+polarimgset(35, 126, 198, 5, 6);
+polarimgset(45, 126, 198, 5, 6);
+
+console.log("Move");
+polarimgset(15, 198, 270, 1, 6);
+polarimgset(25, 198, 270, 3, 6);
+polarimgset(35, 198, 270, 4, 6);
+polarimgset(45, 198, 270, 5, 6);
+
+console.log("Health");
+polarimgset(15, 270, 342, 1, 6);
+polarimgset(25, 270, 342, 3, 6);
+polarimgset(35, 270, 342, 5, 6);
+polarimgset(45, 270, 342, 5, 6);
+
+console.log("Energy");
+polarimgset(15, 342, 414, 1, 6);
+polarimgset(25, 342, 414, 3, 6);
+polarimgset(35, 342, 414, 5, 6);
+polarimgset(45, 342, 414, 5, 6);
