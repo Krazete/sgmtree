2e68a9ae5dcedea1b4b5245f5d1bc76e862e9b07
Ted Michael Celis <krazete@gmail.com>
2021-01-23 02:18:31 -0800

    changed names, fixed dual toggle

--- index.js
+++ index.js
@@ -4,39 +4,52 @@ var radioB = document.getElementById("b");
 var radioS = document.getElementById("s");
 var radioG = document.getElementById("g");
 var radioD = document.getElementById("d");
-var t2f = false;
-var t2s = document.getElementsByClassName("t2");
-var t2b = document.getElementById("t2b");
+
+var dual = false;
+var dualElements = document.getElementsByClassName("dual");
+var dualButton = document.getElementById("dual-button");
 var tree0 = {
     "msg": {},
     "sp": 0,
     "cc": 0,
+    "th": 0,
     "spElement": document.getElementById("sp0"),
-    "ccElement": document.getElementById("cc0")
+    "ccElement": document.getElementById("cc0"),
+    "thElement": document.getElementById("th0")
 };
 var tree1 = {
     "msg": {},
     "sp": 0,
     "cc": 0,
+    "th": 0,
     "spElement": document.getElementById("sp1"),
-    "ccElement": document.getElementById("cc1")
+    "ccElement": document.getElementById("cc1"),
+    "thElement": document.getElementById("th1")
 };
-var dsp = document.getElementById("dsp");
-var dcc = document.getElementById("dcc");
-function toggleT2() {
-    for (var i = 0; i < t2s.length; i++) {
-        if (t2f) {
-            t2b.innerHTML = "Show";
-            t2s[i].classList.add("hidden");
-            t2f = false;
+var dtree = {
+    "spElement": document.getElementById("dsp"),
+    "ccElement": document.getElementById("dcc"),
+    "thElement": document.getElementById("dth")
+};
+var theoniteElements = document.getElementsByClassName("theonite");
+
+function toggleDual() {
+    if (dual) {
+        for (var i = 0; i < dualElements.length; i++) {
+            dualElements[i].classList.add("hidden");
         }
-        else {
-            t2b.innerHTML = "Hide";
-            t2s[i].classList.remove("hidden");
-            t2f = true;
+        dualButton.innerHTML = "Show";
+        dual = false;
+    }
+    else {
+        for (var i = 0; i < dualElements.length; i++) {
+            dualElements[i].classList.remove("hidden");
         }
+        dualButton.innerHTML = "Hide";
+        dual = true;
     }
 }
+
 function setMultiplier() {
     if (radioB.checked) {
         m = 1;
@@ -57,31 +70,48 @@ function setMultiplier() {
     setCost(tree1);
     setDifference();
 }
+
 function msum(total, n) {
-    return total + (isNaN(n) ? 0 : Math.ceil(m * n));
+    return total + Math.ceil(m * n);
 }
+
+function updateElements(tree) {
+    tree.spElement.innerHTML = tree.sp.toLocaleString();
+    tree.ccElement.innerHTML = tree.cc.toLocaleString();
+    tree.thElement.innerHTML = tree.cc.toLocaleString();
+    if (isNaN(tree.th)) {
+        tree.thElement.parentElement.classList.add("hidden");
+    }
+    else {
+        tree.thElement.parentElement.classList.remove("hidden");
+    }
+}
+
 function setCost(tree) {
     if (tree.msg.data) {
         tree.sp = tree.msg.data.sp.reduce(msum, 0);
         tree.cc = tree.msg.data.cc.reduce(msum, 0);
-        tree.spElement.innerHTML = tree.sp.toLocaleString();
-        tree.ccElement.innerHTML = tree.cc.toLocaleString();
+        tree.th = tree.msg.data.th.reduce(msum, 0);
+        updateElements(tree);
     }
 }
+
 function setDifference() {
-    dsp.innerHTML = (tree1.sp - tree0.sp).toLocaleString();
-    dcc.innerHTML = (tree1.cc - tree0.cc).toLocaleString();
+    dtree.sp = tree1.sp - tree0.sp;
+    dtree.cc = tree1.cc - tree0.cc;
+    dtree.th = tree1.th - tree0.th;
+    updateElements(dtree);
 }
+
 function onMessage(msg) {
-    console.log(msg.source.frameElement);
     /* if (msg.origin == "https://krazete.github.io") { */
     if (msg.origin == window.origin) {
         var id = msg.source.frameElement.id;
-        if (id == "t0") {
+        if (id == "svg0") {
             tree0.msg = msg;
             setCost(tree0);
         }
-        else if (id == "t1") {
+        else if (id == "svg1") {
             tree1.msg = msg;
             setCost(tree1);
         }
@@ -89,11 +119,13 @@ function onMessage(msg) {
     }
 }
 
-t2b.addEventListener("click", toggleT2);
 radioZ.addEventListener("input", setMultiplier);
 radioB.addEventListener("input", setMultiplier);
 radioS.addEventListener("input", setMultiplier);
 radioG.addEventListener("input", setMultiplier);
 radioD.addEventListener("input", setMultiplier);
+
+dualButton.addEventListener("click", toggleDual);
+
 window.addEventListener("load", setMultiplier);
 window.addEventListener("message", onMessage);
